80682042849024f2179dad431e62bd56f6f7fe39
Luke Anderson <luke.kansas@gmail.com>
2019-08-10 13:12:25 -0500

    some primitive goemetry

--- src/components/PrimitiveGeometryComponents.js
+++ src/components/PrimitiveGeometryComponents.js
@@ -1,21 +1,41 @@
 import PrimitiveGeometryBase from './PrimitiveGeometryBase';
-import * as PrimitiveGeometry from '../services/Geometry/PrimitiveGeometry';
+import {
+  Box,
+  Circle,
+  Cone,
+  Cylinder,
+  Dodecahedron,
+  Octahedron,
+  Plane,
+  Sphere,
+  Tetrahedron,
+  Triangle,
+  Ring,
+} from '../services/Geometry/PrimitiveGeometry';
 
 class PsVizBox extends PrimitiveGeometryBase {
   static get tag() {
     return 'ps-viz-box';
   }
 
-  static get observedAttributes() {
-    return PrimitiveGeometryBase.observedAttributes;
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Box();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizCircle extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-circle';
   }
 
   connectedCallback() {
     super.connectedCallback();
-    this.geometry = new PrimitiveGeometry.Box();
+    this.geometry = new Circle();
     super.setValuesFromAttributes(PsVizCone.observedAttributes);
     super.connect();
-    console.log('box', this);
   }
 }
 
@@ -24,13 +44,113 @@ class PsVizCone extends PrimitiveGeometryBase {
     return 'ps-viz-cone';
   }
 
-  static get observedAttributes() {
-    return PrimitiveGeometryBase.observedAttributes;
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Cone();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizCylinder extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-cylinder';
   }
 
   connectedCallback() {
     super.connectedCallback();
-    this.geometry = new PrimitiveGeometry.Cone();
+    this.geometry = new Cylinder();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizDodecahedron extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-dodecahedron';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Dodecahedron();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizOctahedron extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-octahedron';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Octahedron();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizPlane extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-plane';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Plane();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizSphere extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-sphere';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Sphere();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizTetrahedron extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-tetrahedron';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Tetrahedron();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizTriangle extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-triangle';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Triangle();
+    super.setValuesFromAttributes(PsVizCone.observedAttributes);
+    super.connect();
+  }
+}
+
+class PsVizRing extends PrimitiveGeometryBase {
+  static get tag() {
+    return 'ps-viz-ring';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.geometry = new Ring();
     super.setValuesFromAttributes(PsVizCone.observedAttributes);
     super.connect();
   }
@@ -38,5 +158,14 @@ class PsVizCone extends PrimitiveGeometryBase {
 
 export default {
   PsVizBox,
+  PsVizCircle,
   PsVizCone,
+  PsVizCylinder,
+  PsVizDodecahedron,
+  PsVizOctahedron,
+  PsVizPlane,
+  PsVizSphere,
+  PsVizTetrahedron,
+  PsVizTriangle,
+  PsVizRing,
 };
--- src/services/Geometry/PrimitiveGeometry.js
+++ src/services/Geometry/PrimitiveGeometry.js
@@ -1,16 +1,40 @@
 import {
   BoxBufferGeometry,
+  BufferAttribute,
+  BufferGeometry,
+  CircleBufferGeometry,
   ConeBufferGeometry,
+  CylinderBufferGeometry,
+  DodecahedronBufferGeometry,
+  DoubleSide,
   FrontSide,
-  MeshLambertMaterial
+  MeshLambertMaterial,
+  OctahedronBufferGeometry,
+  PlaneBufferGeometry,
+  SphereBufferGeometry,
+  TetrahedronBufferGeometry,
+  TorusBufferGeometry,
 } from 'three';
 import BaseGeomtry from './BaseGeometry';
+
+const baseSize = 5;
+const getMaterial = (side = FrontSide) => new MeshLambertMaterial({ side });
 
 export class Box extends BaseGeomtry {
   constructor() {
     super(
-      new BoxBufferGeometry(1, 1, 1),
-      new MeshLambertMaterial({ color: 0x006699, side: FrontSide })
+      new BoxBufferGeometry(baseSize, baseSize, baseSize),
+      getMaterial()
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Circle extends BaseGeomtry {
+  constructor() {
+    super(
+      new CircleBufferGeometry(baseSize, 16),
+      getMaterial(DoubleSide)
     );
     this.paramMap = Object.assign({}, this._baseParams);
   }
@@ -19,8 +43,99 @@ export class Box extends BaseGeomtry {
 export class Cone extends BaseGeomtry {
   constructor() {
     super(
-      new ConeBufferGeometry(5, 20, 32),
-      new MeshLambertMaterial({ color: 0x006699, side: FrontSide })
+      new ConeBufferGeometry(baseSize, 20, 32),
+      getMaterial()
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Cylinder extends BaseGeomtry {
+  constructor() {
+    super(
+      new CylinderBufferGeometry(baseSize, baseSize, baseSize * 2),
+      getMaterial()
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Dodecahedron extends BaseGeomtry {
+  constructor() {
+    super(
+      new DodecahedronBufferGeometry(baseSize),
+      getMaterial()
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Octahedron extends BaseGeomtry {
+  constructor() {
+    super(
+      new OctahedronBufferGeometry(baseSize),
+      getMaterial()
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Plane extends BaseGeomtry {
+  constructor() {
+    super(
+      new PlaneBufferGeometry(baseSize, baseSize),
+      getMaterial(DoubleSide)
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Sphere extends BaseGeomtry {
+  constructor() {
+    super(
+      new SphereBufferGeometry(baseSize),
+      getMaterial()
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Tetrahedron extends BaseGeomtry {
+  constructor() {
+    super(
+      new TetrahedronBufferGeometry(baseSize),
+      getMaterial()
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Triangle extends BaseGeomtry {
+  static getTriangleGeometry(size) {
+    const triangleGeometry = new BufferGeometry();
+    const positions = new Float32Array([
+      -size, -size, 0,
+      -size, size, 0,
+      size, size, 0
+    ])
+    triangleGeometry.addAttribute('position', new BufferAttribute(positions, 3))
+    return triangleGeometry;
+  }
+
+  constructor() {
+    super(
+      Triangle.getTriangleGeometry(baseSize),
+      getMaterial(DoubleSide)
+    );
+    this.paramMap = Object.assign({}, this._baseParams);
+  }
+}
+
+export class Ring extends BaseGeomtry {
+  constructor() {
+    super(
+      new TorusBufferGeometry(baseSize, 1),
+      getMaterial()
     );
     this.paramMap = Object.assign({}, this._baseParams);
   }
